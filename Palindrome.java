d680388455f30281cc6c638da9a1511e397bc938
RAMKUMAR <kumarkumar67221@gmail.com>
2024-09-12 14:27:06 +0530

    Add files via upload

---
+++ Palindrome.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class PalindromeCheck {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a word: ");
+        String original = scanner.nextLine();
+        String reverse = "";
+
+        int length = original.length();
+        for (int i = length - 1; i >= 0; i--) {
+            reverse = reverse + original.charAt(i);
+        }
+
+        if (original.equals(reverse)) {
+            System.out.println(original + " is a palindrome.");
+        } else {
+            System.out.println(original + " is not a palindrome.");
+        }
+        scanner.close();
+    }
+}
